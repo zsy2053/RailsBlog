3a0cc67013bee514c5d29613c4f065b2cd37d265
Shuyang Zang <shuyang.zang@robinsonsolutions.com>
2017-11-30 13:54:40 -0500

    Added user model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129202401) do
+ActiveRecord::Schema.define(version: 20171130185010) do
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
